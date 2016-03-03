1e0aa9684c08a6639d5e7621f1599424fe79e3df
Matt Davidson <matdavid@cisco.com>
2016-03-03 10:08:59 -0500

    csv looks good

--- ansible-report.py
+++ ansible-report.py
@@ -1,11 +1,11 @@
-import os,json,csv,sqlite3,time
+import os,json,sqlite3,time,csv
 
 db_file = "/tmp/ansible.db"
 interesting_modules = ["shell"]
 interesting_data = {"shell": "stdout"}
 header_map = {'shell': 'cmd'}
 
-print "Opening database {}".format(db_file)
+#print "Opening database {}".format(db_file)
 db = sqlite3.connect(db_file)
 cur = db.cursor()
 
@@ -27,16 +27,35 @@ def log(host,play,task,data):
                                 print "Could not write data: %s" % e
 
 def scrub_var(var):
-	return ''.join(chr for chr in var if chr.isalnum())
+        return ''.join(chr for chr in var if chr.isalnum())
 
 def write_csv():
-	global cur
-	with open("results.csv") as csv_file:
-		csv = csv.writer(csv_file)
-		csv_row = []
-		csv.writerow([i[0] for i in cur.description])
-		for row in cur.execute('SELECT time,host,play,task,result FROM log as l1 where time = (SELECT max(time) from log as l2 where l1.host == l2.host and l1.play == l2.play and l1.task == l2.task);'):
-			csv_row = []
+        global cur
+        csv_data = {}
+        keys = []
+        for row in cur.execute('SELECT time,host,play,task,result FROM log as l1 where time = (SELECT max(time) from log as l2 where l1.host == l2.host and l1.play == l2.play and l1.task == l2.task);'):
+                #try:
+                        #print "{0}: {1}".format(str(row[3]),str(row[4]))
+                        host = str(row[1])
+                        task = str(row[3])
+                        result = str(row[4])
+                        if len(csv_data) == 0:
+                                csv_data.update({host: {}})
+                        if host not in csv_data.keys():
+                                csv_data[host] = {}
+                        csv_data[host].update({task: result})
+                        if task not in keys:
+                                #print "Inserting {} into keys".format(task)
+                                keys.append(task)
+                #except Exception as e:
+                #       print "Could not load data: {}".format(e)
+        with open("results.csv",'w+') as csv_file:
+                #print "Data: {}".format(csv_data)
+                #print "Keys {}".format(keys)
+                c = csv.DictWriter(csv_file,fieldnames=keys)
+                c.writeheader()
+                for host in csv_data:
+                        c.writerow(csv_data[host])
 
 
 class CallbackModule():
@@ -64,5 +83,5 @@ class CallbackModule():
                 #print "Starting task {} with data {}".format(task,args)
                 self.current_task = task
 
-#        def playbook_on_stats(self,stats):
-
+        def playbook_on_stats(self,stats):
+                write_csv()
